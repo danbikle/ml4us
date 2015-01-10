2120cfed1c2ca02e504c58cd2770acf14a31ba96
Dan Bikle <dan.bikle@gmail.com>
2015-01-10 06:21:51 +0000

    mo

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root 'syl#index'
   get 'syllabus' => 'syl#index'
   get 'contact'  => 'syl#contact'
+  get 'posts'     => 'posts#index'
+  get 'tags'      => 'posts#tags'
+  get 'questions' => 'questions#index'
+  get 'tags(/:tag)' => 'tags#tag'
   get ':controller(/:action)'
 end
