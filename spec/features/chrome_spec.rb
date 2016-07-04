ee44edc088c6c2229643b65a2d7799901de667fc
Dan Bikle <bikle@bikle.com>
2016-07-04 13:10:38 -0700

    more

---
+++ spec/features/chrome_spec.rb
@@ -0,0 +1,12 @@
+# spec/features/chrome_spec.rb
+# Demo:
+# bin/rspec spec/features/chrome_spec.rb
+require 'rails_helper'
+
+describe 'This should start Chrome', :js => true do
+  Capybara.current_driver = :selenium_chrome
+  it 'should start Chrome' do
+    visit '/about/index'
+    # I should briefly see Chrome; then it should exit.
+  end
+end
