e9b8d0d4d7e0b888586a5656c4f89ec5e2572927
Dan Bikle <bikle@bikle.com>
2016-07-03 23:36:13 -0700

    more

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get  'about/index'
+  root 'about#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
