87a917a154771c94670d359850972cc054a76715
Dan Bikle <bikle@bikle.com>
2016-08-09 14:57:35 -0700

    more

--- spec/features/chrome_spec.rb
+++ spec/features/chrome_spec.rb
@@ -1,11 +1,19 @@
 # spec/features/chrome_spec.rb
 # Demo:
 # bin/rspec spec/features/chrome_spec.rb
+
+# I should have executable:
+# chromedriver
+# in my path.
+# I put it here: ~/bin/chromedriver
+
 require 'rails_helper'
 
 describe 'This should start Chrome', :js => true do
   it 'should start Chrome' do
-    visit '/about/index'
+    visit '/about'
+    visit '/blog'
+    visit '/contact'
     # I should briefly see Chrome; then it should exit.
   end
 end
