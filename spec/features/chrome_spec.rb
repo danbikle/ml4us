5a41f1026fbda9fba5ebbd7b1067008071f75fe3
Dan Bikle <bikle@bikle.com>
2016-08-09 23:58:47 -0700

    more

--- spec/features/chrome_spec.rb
+++ spec/features/chrome_spec.rb
@@ -10,13 +10,46 @@
 require 'rails_helper'
 
 describe 'This should start Chrome', :js => true do
-  it 'should start Chrome' do
+  it 'should visit some links' do
     visit '/about'
+    sleep 1
     visit '/blog'
+    sleep 1
     visit '/contact'
+    sleep 1
     visit '/cclasses/class01'
+    sleep 1
+    visit '/cclasses/class01at'
+    sleep 1
+    visit '/cclasses/class01ch'
+    sleep 1
+    visit '/cclasses/class01cp'
+    sleep 1
+    visit '/cclasses/class01ml'
+    sleep 1
+    visit '/cclasses/class01ova'
+    sleep 1
+    visit '/cclasses/class01rl'
+    sleep 1
+    visit '/cclasses/class01rs2'
+    sleep 1
+    visit '/cclasses/class01rs'
+    sleep 1
+    visit '/cclasses/class01sl'
+    sleep 1
+    visit '/cclasses/class01td'
+    sleep 1
+    visit '/cclasses/class01uc'
+    sleep 1
+    visit '/cclasses/class01vb'
+    sleep 1
     visit '/cclasses/class02'
+    sleep 1
     visit '/cclasses/class03'
-    # I should briefly see Chrome; then it should exit.
+    sleep 1
+    # I should see Chrome; then it should exit.
   end
 end
+
+
+
