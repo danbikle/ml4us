1d2aed4cde4d41db2dfddaf23b472c439532dd8c
Dan Bikle <bikle@bikle.com>
2016-08-18 22:01:09 -0700

    more

--- public/class0312.py
+++ public/class0312.py
@@ -32,5 +32,7 @@ cp2016_df['sl'] = sl_l
 
 # Calculate squared errors:
 cp2016_df['sqe'] = (cp2016_df.Close - cp2016_df.sl)**2
-pdb.set_trace()
-cp2016_df.head()
+
+rmse_f = np.sqrt(np.mean(cp2016_df.sqe))
+print(rmse_f)
+
