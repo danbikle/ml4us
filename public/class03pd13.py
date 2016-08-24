47a82d7fc117937a5ed4ab38a06ab219d5f926f3
Dan Bikle <bikle@bikle.com>
2016-08-23 23:59:42 -0700

    more

--- public/class03pd13.py
+++ public/class03pd13.py
@@ -3,7 +3,7 @@
 # This script should mimic the SQL listed below:
 
 # SELECT * FROM prices WHERE cdate = (SELECT MAX(cdate) FROM prices);              
-# SELECT cdate,closep FROM prices WHERE cdate = (SELECT MAX(cdate)-1 FROM prices); 
+# SELECT cdate,closep FROM prices WHERE cdate = (SELECT MAX(cdate)-1 FROM prices);
 # SELECT cdate,closep FROM prices WHERE cdate > (SELECT MAX(cdate)-10 FROM prices);
 
 import pandas as pd
@@ -24,6 +24,15 @@ prices_df['cdate'] = pd.to_datetime(prices_df.cdate_s)
 arg_max_cdate      = prices_df.cdate.argmax()
 print(prices_df.iloc[arg_max_cdate])
 
+# Mimic
+# SELECT cdate,closep FROM prices WHERE cdate = (SELECT MAX(cdate)-1 FROM prices);
+
+from datetime import timedelta
+maxdate   = prices_df.iloc[arg_max_cdate].cdate
+maxdate_1 = maxdate - timedelta(days=1)
+pred_sr   = (prices_df.cdate == maxdate_1)
+myrow     = prices_df[['cdate','closep']][pred_sr]
+print(myrow)
 
 'bye'
 
