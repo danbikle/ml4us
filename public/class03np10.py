8e9faa8a7335155e2348a54b8c318a6f15bb5afb
Dan Bikle <bikle@bikle.com>
2016-08-26 14:05:53 -0700

    more

---
+++ public/class03np10.py
@@ -0,0 +1,14 @@
+# class03np10.py
+
+# Create a NumPy Array From Pandas DataFrame of GSPC prices.
+
+import pandas as pd
+import numpy  as np
+
+prices_df = pd.read_csv('http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC')
+prices_a  = np.array(prices_df)
+
+# I should print the first 9 rows to help me see the NumPy Array:
+print(prices_a[:9])
+
+'bye'
