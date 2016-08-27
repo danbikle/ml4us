d773f20da0ecd712b9cab070c2f670b5cd1d7ba4
Dan Bikle <bikle@bikle.com>
2016-08-27 11:36:26 -0700

    hello

--- public/class03pd45.py
+++ public/class03pd45.py
@@ -3,7 +3,7 @@
 # This script should slice and dice
 
 import pandas as pd
-
+import numpy  as np
 prices_df = pd.read_csv('http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC')
 prices_df.columns = ['cdate_s','openp','highp','lowp','closep','volume','adjp']
 
@@ -14,6 +14,9 @@ s1_df
 
 s2_df = prices_df[5:11][['cdate_s','closep']]
 s2_df
-
-
+import pdb
+pdb.set_trace()
+prices_a = np.array(prices_df)
+s1_a = prices_a[5:11,[0,4]]
+s1_a
 'bye'
