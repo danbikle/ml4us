47843696d86cc7cea5f53a79e6cd00f1dadf6069
Dan Bikle <bikle@bikle.com>
2016-08-28 00:24:09 -0700

    hello

---
+++ public/class03np15.py
@@ -0,0 +1,28 @@
+# class03np15.py
+
+# This script should slice and dice
+
+import pandas as pd
+import numpy  as np
+
+prices_df = pd.read_csv('http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC')
+
+# I should do it the numpy way:
+prices_a = np.array(prices_df)
+
+# I should get all rows where column-0 > '2016-07'
+pred1_a  = (prices_a[:,0] > '2016-07')
+# I should get all rows where column-0 < '2016-08'
+pred2_a  = (prices_a[:,0] < '2016-08')
+
+# I should combine the two predicates:
+preds_a = pred1_a & pred2_a
+
+# I should slice out july:
+july_a   = prices_a[preds_a]
+
+# I should get all rows and get columns 0 and 4:
+s1_a     = july_a[:,[0,4]]
+print(s1_a)
+
+'bye'
