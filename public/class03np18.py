7e05a827f17304ac59aa14f37d8374e4669ab30e
Dan Bikle <bikle@bikle.com>
2016-08-28 23:44:48 -0700

    hello

---
+++ public/class03np18.py
@@ -0,0 +1,16 @@
+# class03np18.py
+
+# This script should compute columns pctlag1 and pctlead from closep.
+
+import pandas as pd
+import numpy  as np
+
+import pdb
+
+prices_df = pd.read_csv('http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC')
+prices_df.columns = ['cdate_s','openp','highp','lowp','closep','volume','adjp']
+
+# I should get 2016 July and two columns:
+
+
+'bye'
