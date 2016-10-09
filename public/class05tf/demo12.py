7ca52573929cc127ba5050bdb891aa7e18609eef
Dan Bikle <bikle@bikle.com>
2016-10-09 13:50:59 -0700

    more

--- public/class05tf/demo12.py
+++ public/class05tf/demo12.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 import numpy      as np
 
 # Create phony x, y data points in NumPy, y = x * 0.1 + 0.3
-x_data = np.random.rand(3).astype(np.float32)
+x_data = np.random.rand(5).astype(np.float32)
 y_data = x_data * 0.1 + 0.3
 
 # Try to find values for W and b that compute y_data = W * x_data + b
@@ -18,9 +18,9 @@ b = tf.Variable(tf.zeros([1]))
 y = W * x_data + b
 
 # Minimize the mean squared errors.
-loss = tf.reduce_mean(tf.square(y - y_data))
+loss      = tf.reduce_mean(tf.square(y - y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
-train = optimizer.minimize(loss)
+train     = optimizer.minimize(loss)
 
 # Before starting, initialize the variables.  We will 'run' this first.
 init = tf.initialize_all_variables()
@@ -30,7 +30,7 @@ sess = tf.Session()
 sess.run(init)
 
 import pdb
-pdb.set_trace()
+
 x_data
 y_data
 tf_W = sess.run(W)
@@ -38,12 +38,46 @@ tf_b = sess.run(b)
 tf_y = sess.run(y)
 tf_loss = sess.run(loss)
 
+def my_loss(y0_a, y1_a):
+    diff_a = y0_a - y1_a
+    sq_a   = [e_f*e_f for e_f in diff_a]
+    return np.mean(sq_a)
+print('my_loss:')
+print(my_loss(y_data,tf_y))
+
+print('tf_loss:')
+print(tf_loss)
+
+# I should create lists to collect artifacts of optimizer:
+w_l = []
+b_l = []
+l_l = []
 
 # Fit the line.
-for step in range(21):
+for step in range(5):
     sess.run(train)
-    if step % 4 == 0:
-        print(step, sess.run(W), sess.run(b))
+    tf_W = sess.run(W)
+    tf_b = sess.run(b)
+    tf_y = sess.run(y)
+    tf_loss = sess.run(loss)
+    print('tf_loss:')
+    print(tf_loss)
+    w_l.append(tf_W)
+    b_l.append(tf_b)
+    l_l.append(tf_loss)
+print(w_l)
+print(b_l)
+print(l_l)
 
-# Learns best fit is W: [0.1], b: [0.3]
-
+# I should create lists to collect artifacts of optimizer:
+dw_l = []
+db_l = []
+dl_l = []
+pdb.set_trace()
+for i_i in range(len(w_l)-1):
+  print(i_i)
+  dw_l.append(w_l[i_i+1]-w_l[i_i])
+  db_l.append(b_l[i_i+1]-b_l[i_i])
+  dl_l.append(l_l[i_i+1]-l_l[i_i])
+  
+'bye'
