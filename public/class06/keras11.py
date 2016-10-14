0fcb3eea699e7def04b1012b15193fd4eb3f4cb9
Dan Bikle <bikle@bikle.com>
2016-10-14 02:10:18 -0700

    more

--- public/class06/keras11.py
+++ public/class06/keras11.py
@@ -37,11 +37,29 @@ c2_df = iris1_df.copy()[(iris1_df.label == 'Iris-versicolor')]
 c3_df = iris1_df.copy()[(iris1_df.label == 'Iris-virginica' )]
 
 # I should change the label from a string to an integer:
+
 c1_df['label'] = 1
 c2_df['label'] = 2
 c3_df['label'] = 3
+
 iris2_df = pd.concat([c1_df,c2_df,c3_df])
-print(iris2_df.head())
-print(iris2_df.tail())
+
+X_train = iris2_df[['f1','f2','f3','f4']]
+
+# from keras.utils import np_utils
+# Y_train = np_utils.to_categorical(iris2_df.label,nb_classes=3)
+y_l = []
+for y_i in iris2_df.label:
+    if y_i == 1:
+        y_l.append([1,0,0])
+    elif y_i == 2:
+        y_l.append([0,1,0])
+    else:
+        y_l.append([0,0,1])
+
+        
+pdb.set_trace()
+Y_train= np.array(y_l)
+#model.fit(X_train, Y_train, nb_epoch=5, batch_size=32)
 
 'bye'
