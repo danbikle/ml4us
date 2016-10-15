239d39221f67eb358073a5a95a7ee5ebfa24347c
Dan Bikle <bikle@bikle.com>
2016-10-14 22:36:08 -0700

    more

--- script/railss.bash
+++ script/railss.bash
@@ -4,8 +4,7 @@
 
 # This script should start rails server on all IPs so I can see the app from another host.
 
-cd ~/ml4us/
-bin/rails s -b 0.0.0.0
+`dirname $0`/../bin/rails s -p 3540 -b 0.0.0.0
+
 # 0.0.0.0 declares that I want rails listening on all IPs bound to this host.
 exit
-
