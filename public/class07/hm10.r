07b0c4f1c2d122dccc455d92621ffb946816516a
Dan Bikle <bikle@bikle.com>
2016-10-23 07:20:10 -0700

    more

---
+++ public/class07/hm10.r
@@ -0,0 +1,14 @@
+# hm10.r
+
+# Ref:
+# https://flowingdata.com/2010/01/21/how-to-make-a-heatmap-a-quick-and-easy-solution/
+
+# Demo:
+# R -f hm10.r
+
+# I should load nba data:
+nba <- read.csv("http://datasets.flowingdata.com/ppg2008.csv", sep=",")
+# I should order by PTS column:
+nba <- nba[order(nba$PTS),]
+
+nba
