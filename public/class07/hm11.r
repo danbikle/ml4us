5f69b0186810d1f0e1f3d32e4889636733f7d082
Dan Bikle <bikle@bikle.com>
2016-10-23 15:59:03 -0700

    more

--- public/class07/hm11.r
+++ public/class07/hm11.r
@@ -9,7 +9,16 @@
 
 # I should get GSPC dates and prices:
 gspc0_df = read.csv('http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC')
-head(gspc0_df)
-write.csv(gspc0_df,'gspc0_df.csv')
+
+# I should identify each row by Date instead of an integer:
+# row.names(gspc0_df) = gspc0_df$Date
+
+# I should order by Date:
+gspc1_df = gspc0_df[order(gspc0_df$Date),]
+
+head(gspc1_df)
+
+# I should write the df to a csv:
+write.csv(gspc1_df,'gspc1_df.csv')
 
 'bye'
