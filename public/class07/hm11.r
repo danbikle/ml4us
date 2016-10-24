c18d6005333db218fcd1c0f03998cdcbf3ffe8d7
Dan Bikle <bikle@bikle.com>
2016-10-24 00:49:05 -0700

    more

--- public/class07/hm11.r
+++ public/class07/hm11.r
@@ -27,9 +27,6 @@ gspc3_df = data.frame(gspc2_df$Date,gspc2_df$Close)
 
 colnames(gspc3_df) = c('cdate','cp')
 
-head(gspc3_df)
-tail(gspc3_df)
-
 # I should compute pctlead,pctlag1 from cp
 len_i            = length(gspc3_df$cp)
 last_f           = gspc3_df$cp[len_i]
@@ -39,48 +36,39 @@ gspc3_df$pctlag1 = c(0, gspc3_df$pctlead)[1:len_i]
 
 # I should get Day-of-Week, Month-of-Year from cdate:
 gspc3_df$dow = format(as.Date(gspc3_df$cdate),"%w")
+# I should convert moy to integer so I can sort it:
 gspc3_df$moy = strtoi(format(as.Date(gspc3_df$cdate),"%-m"))
 
 # I should get rows after 1990-01-01:
-pred_v = (as.Date(gspc3_df$cdate) > '1990-01-01')
-tail(pred_v)
+pred_v   = (as.Date(gspc3_df$cdate) > '1990-01-01')
 gspc4_df = gspc3_df[ pred_v , ]
 
-head(gspc4_df)
-tail(gspc4_df)
-
-# I should use aggregate() to sum(pctlead) groupby Month-of-Year:
-agg_moy = aggregate(pctlead ~ moy, gspc4_df, sum)
-print('Sum of pctlead groupby Month-of-Year:')
-print(agg_moy)
-
-# I should use aggregate() to sum(pctlead) groupby Day-of-Week:
-agg_dow = aggregate(pctlead ~ dow, gspc4_df, sum)
-print('Sum of pctlead groupby Day-of-Week:')
-print(agg_dow)
+# I should save this data for other scripts:
+write.csv(gspc4_df,'gspc4.csv', row.names=FALSE)
 
 # I should use aggregate() to sum(pctlead) groupby Day-of-Week and Month-of-Year:
 dowmoy0_df = aggregate(gspc4_df$pctlead, by=list(dow=gspc4_df$dow,moy=gspc4_df$moy),sum)
 
+# I should use better colnames:
 colnames(dowmoy0_df) = c('dow','moy','sum_pctlead')
-print(dowmoy0_df)
 
 # I should order by dow:
 dowmoy1_df = dowmoy0_df[order(dowmoy0_df$dow),]
-dowmoy1_df
 
-# I should build a matrix for a heatmap:
-
+# Above DF is long and skinny.
+# The days are vertically stacked.
+# I should slice out each day:
 mon = dowmoy1_df[(dowmoy1_df$dow == 1),3]
 tue = dowmoy1_df[(dowmoy1_df$dow == 2),3]
 wed = dowmoy1_df[(dowmoy1_df$dow == 3),3]
 thu = dowmoy1_df[(dowmoy1_df$dow == 4),3]
 fri = dowmoy1_df[(dowmoy1_df$dow == 5),3]
 
+# I should horizontally stack the days:
 hm1_df = data.frame(mon,tue,wed,thu,fri)
 
+# I should build a matrix for a heatmap:
 hm1_x = data.matrix(hm1_df)
-hm1_x
 
 png('hm11.png',width=800, units='px', pointsize=22, height=1100)
 
