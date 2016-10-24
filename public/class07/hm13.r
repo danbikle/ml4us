23e487c5f3906eea35e2f7eb57cf5ac866839895
Dan Bikle <bikle@bikle.com>
2016-10-24 15:42:25 -0700

    more

--- public/class07/hm13.r
+++ public/class07/hm13.r
@@ -21,10 +21,8 @@
 # # I should write the df to a csv:
 # write.csv(gspc1_df,'gspc1_df.csv', row.names=FALSE)
 
-gspc2_df = read.csv('gspc1_df.csv')
-
-gspc3_df = data.frame(gspc2_df$Date,gspc2_df$Close)
-
+gspc2_df           = read.csv('gspc1_df.csv')
+gspc3_df           = data.frame(gspc2_df$Date,gspc2_df$Close)
 colnames(gspc3_df) = c('cdate','cp')
 
 # I should compute pctlead,pctlag1 from cp
@@ -37,7 +35,6 @@ gspc3_df$pctlag1 = c(0, gspc3_df$pctlead)[1:len_i]
 # I should get Month-of-Year, Day-of-Week from cdate.
 # I should get a column like this: 01_2 which corresponds to January_Tuesday.
 gspc3_df$moydow = format(as.Date(gspc3_df$cdate),"%m_%w")
-tail(gspc3_df)
 
 # I should get rows after 1990-01-01:
 pred_v   = (as.Date(gspc3_df$cdate) > '1990-01-01')
@@ -46,20 +43,17 @@ gspc4_df = gspc3_df[ pred_v , ]
 # I should use aggregate() to sum(pctlead) groupby Month-of-Year, Day-of-Week:
 moydow0_df = aggregate(pctlead ~ moydow, gspc4_df, sum)
 
-# I should order by moydow:
-moydow1_df            = moydow0_df[order(moydow0_df$moydow),]
-moydow1_df$pl2        = moydow1_df$pctlead
+# I should order by moydow descending:
+moydow1_df            = moydow0_df[order(moydow0_df$moydow,decreasing=TRUE),]
+moydow1_df$o          = moydow1_df$pctlead
 row.names(moydow1_df) = moydow1_df$moydow
-head(moydow1_df)
 
 # I should get a matrix for the heatmap:
 moydow_matrix = data.matrix(moydow1_df)[,2:3]
-moydow_matrix
 
 # I should show a heatmap:
 png('moydow.png')#,width=400, units='px', pointsize=22, height=3100)
 moydow = heatmap(moydow_matrix, Rowv=NA, Colv=NA, scale="column",col = rainbow(8, start=0, end=2/6)  )
 dev.off()
-
 
 'bye'
