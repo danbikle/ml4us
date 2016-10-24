2e818263c288e790f5c29f1bbc8906850d0113ba
Dan Bikle <bikle@bikle.com>
2016-10-24 00:26:05 -0700

    more

--- public/class07/hm11.r
+++ public/class07/hm11.r
@@ -39,7 +39,7 @@ gspc3_df$pctlag1 = c(0, gspc3_df$pctlead)[1:len_i]
 
 # I should get Day-of-Week, Month-of-Year from cdate:
 gspc3_df$dow = format(as.Date(gspc3_df$cdate),"%w")
-gspc3_df$moy = format(as.Date(gspc3_df$cdate),"%-m")
+gspc3_df$moy = strtoi(format(as.Date(gspc3_df$cdate),"%-m"))
 
 # I should get rows after 1990-01-01:
 pred_v = (as.Date(gspc3_df$cdate) > '1990-01-01')
@@ -60,11 +60,30 @@ print('Sum of pctlead groupby Day-of-Week:')
 print(agg_dow)
 
 # I should use aggregate() to sum(pctlead) groupby Day-of-Week and Month-of-Year:
-agg_dowmoy = aggregate(gspc4_df$pctlead, by=list(dow=gspc4_df$dow,moy=gspc4_df$moy),sum)
+dowmoy0_df = aggregate(gspc4_df$pctlead, by=list(dow=gspc4_df$dow,moy=gspc4_df$moy),sum)
 
-colnames(agg_dowmoy) = c('dow','moy','sum_pctlead')
+colnames(dowmoy0_df) = c('dow','moy','sum_pctlead')
+print(dowmoy0_df)
 
-print(agg_dowmoy)
+# I should order by dow:
+dowmoy1_df = dowmoy0_df[order(dowmoy0_df$dow),]
+dowmoy1_df
+
+# I should build a matrix for a heatmap:
+
+mon = dowmoy1_df[(dowmoy1_df$dow == 1),3]
+tue = dowmoy1_df[(dowmoy1_df$dow == 2),3]
+wed = dowmoy1_df[(dowmoy1_df$dow == 3),3]
+thu = dowmoy1_df[(dowmoy1_df$dow == 4),3]
+fri = dowmoy1_df[(dowmoy1_df$dow == 5),3]
+
+
+hm1_df = data.frame(mon,tue,wed,thu,fri)
+hm1_df
+
+#dowmoy_sum_matrix = data.matrix(dowmoy_df)
+#dowmoy_sum_matrix
+
 
 
 'bye'
