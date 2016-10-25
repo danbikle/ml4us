39a867a6325f6311afafee4f3e7af5e074c5ff41
Dan Bikle <bikle@bikle.com>
2016-10-24 21:29:32 -0700

    more

--- public/class07/hm13.r
+++ public/class07/hm13.r
@@ -40,23 +40,30 @@ gspc3_df$moydow = format(as.Date(gspc3_df$cdate),"%m_%w")
 pred_v   = (as.Date(gspc3_df$cdate) > '1990-01-01')
 gspc4_df = gspc3_df[ pred_v , ]
 
+# I should get rows where pctlag1 < 0:
+down_v = (gspc4_df$pctlag1 < 0)
+
+# I should get rows where pctlag1 >= 0:
+up_v = (gspc4_df$pctlag1 >=0)
+
 # I should use aggregate() to sum(pctlead) groupby Month-of-Year, Day-of-Week:
-moydow0_df = aggregate(pctlead ~ moydow, gspc4_df, sum)
+moydow0_d_df = aggregate(pctlead ~ moydow, gspc4_df[down_v,], sum)
+moydow0_u_df = aggregate(pctlead ~ moydow, gspc4_df[up_v,  ], sum)
 
 # I should order by moydow descending:
-moydow1_df            = moydow0_df[order(moydow0_df$moydow,decreasing=TRUE),]
-moydow1_df$o          = moydow1_df$pctlead
-row.names(moydow1_df) = moydow1_df$moydow
+moydow1_d_df   = moydow0_d_df[order(moydow0_d_df$moydow,decreasing=TRUE),]
+moydow1_u_df   = moydow0_u_df[order(moydow0_u_df$moydow,decreasing=TRUE),]
+
+# I should hstack them:
+d_pctlead = moydow1_d_df$pctlead
+u_pctlead = moydow1_u_df$pctlead
+moydow_df = data.frame(d_pctlead,u_pctlead)
+
+# I should prepare df for feeding it to heatmap():
+row.names(moydow_df) = moydow1_d_df$moydow
+moydow_x             = data.matrix(moydow_df)
 
 # I should report:
-moydow1_df[order(moydow1_df$moydow),1:2]
-
-# I should get a matrix for the heatmap:
-moydow_matrix = data.matrix(moydow1_df)[,2:3]
-
-# I should show a heatmap:
-png('moydow.png')#,width=400, units='px', pointsize=22, height=3100)
-moydow = heatmap(moydow_matrix, Rowv=NA, Colv=NA, scale="column",col = rainbow(8, start=0, end=2/6)  )
-dev.off()
+moydow_x
 
 'bye'
