28821408d3f201e19d62247179297f0a042549a3
Dan Bikle <bikle@bikle.com>
2016-10-28 20:38:52 -0700

    more

--- public/class07/get_predictions.r
+++ public/class07/get_predictions.r
@@ -11,22 +11,13 @@
 # R -f get_predictions.r
 
 get_prediction = function(dt_s){
-  return(1.1)
-}
-
-feat_df = read.csv('feat.csv')
-
-dates_v = tail(feat_df$cdate)
-
-for (dt_s in dates_v){
   # I should get value of YYYY so I can locate modelYYYY.csv
-  yr_s  = format(as.Date(dt_s),"%Y")
-  csv_s = paste('model',yr_s,'.csv', sep='')
+  yr_s     = format(as.Date(dt_s),"%Y")
+  csv_s    = paste('model',yr_s,'.csv', sep='')
   # I should get value of pctlag, moydow from feat_df using dt_s.
   row_v    = (feat_df$cdate == dt_s)
   pctlag_f = feat_df[row_v,]$pctlag
   moydow_s = feat_df[row_v,]$moydow
-  print(feat_df[row_v,])
   # I should use pctlag_f to pick which column in the model csv I want:
   col_i = 2
   if (pctlag_f > 0){
@@ -34,10 +25,19 @@ for (dt_s in dates_v){
   }
   # I should use moydow to get a row from model:
   model_df = read.csv(csv_s)
-  pred_v = (moydow_s == model_df$moydow)
-  print(model_df[pred_v ,])
-  print(model_df[pred_v , col_i])
-  stophere
+  pred_v   = (moydow_s == model_df$moydow)
+  return(model_df[pred_v , col_i])
 }
+
+feat_df = read.csv('feat.csv')
+
+dates_v = tail(feat_df$cdate)
+
+predictions_v = c()
+for (dt_s in dates_v){
+  predictions_v = c(predictions_v,  get_prediction(dt_s))
+}
+
+print(predictions_v)
 
 'bye'
