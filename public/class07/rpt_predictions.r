e1bd4d483b23e867cad3277af8fa4c75c586535a
Dan Bikle <bikle@bikle.com>
2016-10-30 12:59:34 -0700

    more

--- public/class07/rpt_predictions.r
+++ public/class07/rpt_predictions.r
@@ -10,13 +10,38 @@
 predictions_df = read.csv('predictions.csv')
 
 # I should report Long-Only Effectiveness:
-sum(predictions_df$pctlead)
+long_only_eff_f = sum(predictions_df$pctlead)
+print('long_only_eff:')
+print(long_only_eff_f)
 
 # I should calculate Model Effectiveness
 predictions_df$effectiveness = sign(predictions_df$prediction) * predictions_df$pctlead
-head(predictions_df)
 
 # I should report Model Effectiveness:
-sum(predictions_df$effectiveness)
+model_eff_f = sum(predictions_df$effectiveness)
+print('model_eff:')
+print(model_eff_f)
+
+# I should calculate Long-Only Accuracy.
+pctlead_up_v = sign(predictions_df$pctlead)
+# I should count the 1 values
+lo_count_up_i = sum(pctlead_up_v == 1)
+# Accuracy should be count of up days divided by all days:
+lo_accuracy_f = 100.0 * lo_count_up_i / length(predictions_df$pctlead)
+print('Long-Only accuracy:')
+print(lo_accuracy_f)
+
+# I should calculate model Accuracy.
+# If the signs match, that should be accurate:
+predictions_df$accuracy = sign(predictions_df$prediction) * sign(predictions_df$pctlead)
+
+# I should count the 1 values
+tf_v = (predictions_df$accuracy == 1)
+# I should use sum() to sum the 1 values which should be same as count of TRUE values.
+true_count_i = sum(tf_v)
+all_count_i  = length(predictions_df$accuracy)
+accuracy_f   = 100.0 * true_count_i / all_count_i
+print('model accuracy:')
+print(accuracy_f)
 
 'bye'
