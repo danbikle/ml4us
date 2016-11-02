9083d87f05062bfc9a87a760fb6523ce44169800
Dan Bikle <bikle@bikle.com>
2016-11-01 23:41:30 -0700

    more

--- public/class08/f10.py
+++ public/class08/f10.py
@@ -6,22 +6,18 @@ import pandas as pd
 from datetime import datetime
 
 f0_df = pd.read_csv('small.csv', names=['pair','ts0','bid','ask'])
-print(f0_df.tail())
 
-f1_df = f0_df.copy()[['pair','ts0']]
+f1_df       =  f0_df.copy()[['pair','ts0']]
 f1_df['cp'] = (f0_df.bid+f0_df.ask)/2
-
-
-ts1_l = [ts[:14] for ts in f1_df.ts0]
-print(ts1_l[-3:])
-
-ts2_sr = pd.to_datetime(ts1_l, utc=True)
-print(ts2_sr[-3:])
-
+ts1_l       = [ts[:14] for ts in f1_df.ts0]
+ts2_sr      = pd.to_datetime(ts1_l, utc=True)
 f1_df['ts'] = [5*60*int(int(ts.strftime("%s"))/5/60) for ts in ts2_sr]
-
-f2_df = f1_df.copy()[['pair','ts','cp']]
+f2_df       = f1_df.copy()[['ts','cp']]
 print(f2_df.tail())
+f3_df = f2_df.groupby(['ts']).cp.mean()
+print(f3_df.head())
+print(f3_df.tail())
 
+f3_df.to_csv('eur.csv', float_format='%4.6f', index=False)
 
 'bye'
