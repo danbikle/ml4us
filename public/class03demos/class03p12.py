ced70d418e87392a029fa6b124379abc78141bac
Dan Bikle <bikle@bikle.com>
2017-06-15 19:45:48 -0700

    more

--- public/class03demos/class03p12.py
+++ public/class03demos/class03p12.py
@@ -1,18 +1,20 @@
-# class03p12.py
+"""
+class03p12.py
 
-# This script should calculate RMSE of straight line between first and last prices of 2016.
+This script should calculate RMSE of straight line between first and last prices of 2016.
+"""
 
 import pandas as pd
 import numpy  as np
 
-csvfile   = 'http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC'
-cp_df     = pd.read_csv(csvfile).sort_values(['Date'])
-cp2016_sr = (cp_df.Date > '2016') & (cp_df.Date < '2017')
-cp2016_df = cp_df[['Date','Close']][cp2016_sr]
+csvfile   = 'http://spy611.herokuapp.com/csv/allpredictions.csv'
+cp_df     = pd.read_csv(csvfile).sort_values(['cdate'])
+cp2016_sr = (cp_df.cdate > '2016') & (cp_df.cdate < '2017')
+cp2016_df = cp_df[['cdate','cp']][cp2016_sr]
 x1x0_i    = len(cp2016_df)
-y1y0_f    = cp2016_df.iloc[-1].Close-cp2016_df.iloc[0].Close
+y1y0_f    = cp2016_df.iloc[-1].cp-cp2016_df.iloc[0].cp
 m_f       = y1y0_f / x1x0_i
-b_f       = cp2016_df.iloc[0].Close
+b_f       = cp2016_df.iloc[0].cp
 # My equation for straight line:
 def yval(x_in):
     return m_f * x_in + b_f
@@ -23,7 +25,7 @@ cp2016_df['sl'] = yvals_l
 
 # Goog: In Pandas how to combine columns?
 # I should square the difference of each error:
-sqdiffe = (cp2016_df.Close - cp2016_df.sl)**2
+sqdiffe = (cp2016_df.cp - cp2016_df.sl)**2
 
 # I should find mean and then sqrt:
 print('RMSE between straight line and closing price:')
