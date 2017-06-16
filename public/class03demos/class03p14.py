8d39e974b1fd4e5e67b96adb288d3ad47d129a56
Dan Bikle <bikle@bikle.com>
2017-06-16 00:15:18 -0700

    more

--- public/class03demos/class03p14.py
+++ public/class03demos/class03p14.py
@@ -1,16 +1,19 @@
-# class03p14.py
+"""
+class03p14.py
 
-# This script should calculate a prediction.
+This script should calculate a prediction.
 
-# I should predict the price 70 days after the first price.
+I should predict the price 70 days after the first price.
 
-# ref:
-# http://www.ml4.us/class03/pdf1.png
-# http://www.stat.purdue.edu/~jennings/stat514/stat512notes/topic3.pdf
+ref:
+http://www.ml4.us/class03/pdf1.png
+http://www.stat.purdue.edu/~jennings/stat514/stat512notes/topic3.pdf
+"""
 
 import numpy  as np
 
-beta_l = [[1.90196202e+03],[1.82178868e+00]]
+
+beta_l = [[1.93878278e+03], [1.24198003e+00]]
 
 # The first column of X-matrix is always 1:
 xval_l = [[1,70]]
@@ -19,6 +22,9 @@ yhat_f = np.matmul(xval_l,beta_l)
 
 print('I predict the price 70 days after the first price to be:')
 print(yhat_f)
+
+print('Using scalars, I predict the price 70 days after the first price to be:')
+print(str(1938.78 + 1.24198 * 70.0))
 
 'bye'
 
