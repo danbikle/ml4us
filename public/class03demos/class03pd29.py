ca18af76abc78c4041aeeffc851d44ae3ccdf483
Dan Bikle <bikle@bikle.com>
2017-06-17 00:19:30 -0700

    more

--- public/class03demos/class03pd29.py
+++ public/class03demos/class03pd29.py
@@ -1,23 +1,27 @@
-# class03pd29.py
+"""
+class03pd29.py
 
-# This script should mimic the SQL listed below:
+This script should mimic the SQL listed below:
 
-# -- Update some rows of a column:
-# UPDATE prices3 SET diff = 1 WHERE cdate = '2016-08-10';
-# 
-# -- Update column using other columns:
-# UPDATE prices3 SET diff = closep - openp;
+-- Update some rows of a column:
+UPDATE prices3 SET diff = 1 WHERE cdate = '2016-08-10';
+
+-- Update column using other columns:
+UPDATE prices3 SET diff = closep - openp;
+"""
 
 # SQL lacks the ability to get data from the web.
 # Pandas can get data from the web:
 
 import pandas as pd
 
-prices_df = pd.read_csv('http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC')
-prices_df.columns = ['cdate_s','openp','highp','lowp','closep','volume','adjp']
+prices_df         = pd.read_csv('http://ml4.us/csv/ibm.csv')
+prices_df.columns = ['cdate_s', 'openp', 'highp', 'lowp', 'closep', 'adjp', 'volume']
 
-# I should setup prices3:
-pred1_sr   = prices_df.cdate_s > '2016-08-08'
+# I should setup prices3_df:
+pred1a_sr  = prices_df.cdate_s > '2016-08-08'
+pred1b_sr  = prices_df.cdate_s < '2016-09-01'
+pred1_sr   = (pred1a_sr & pred1b_sr)
 prices3_df = prices_df.copy()[['cdate_s','openp','closep']][pred1_sr]
 prices3_df['diff'] = prices3_df.closep - prices3_df.openp
 
