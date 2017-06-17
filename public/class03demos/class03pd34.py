04b9ace64b67415c58d1af819effc78c59dab37e
Dan Bikle <bikle@bikle.com>
2017-06-17 00:32:37 -0700

    more

--- public/class03demos/class03pd34.py
+++ public/class03demos/class03pd34.py
@@ -1,28 +1,33 @@
-# class03pd34.py
+"""
+class03pd34.py
 
-# This script should mimic the SQL listed below:
-# 
-# -- Delete a row:
-# DELETE FROM prices3 WHERE cdate = '2016-08-10';
-# 
-# -- Delete some rows:
-# DELETE FROM prices3 WHERE cdate < '2016-08-10';
-# 
-# -- Delete all rows:
-# SELECT COUNT(*) FROM prices3; 
-# DELETE          FROM prices3;
-# SELECT COUNT(*) FROM prices3; 
+This script should mimic the SQL listed below:
+
+-- Delete a row:
+DELETE FROM prices3 WHERE cdate = '2016-08-10';
+
+-- Delete some rows:
+DELETE FROM prices3 WHERE cdate < '2016-08-10';
+
+-- Delete all rows:
+SELECT COUNT(*) FROM prices3; 
+DELETE          FROM prices3;
+SELECT COUNT(*) FROM prices3; 
+"""
 
 # SQL lacks the ability to get data from the web.
 # Pandas can get data from the web:
 
 import pandas as pd
 
-prices_df = pd.read_csv('http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC')
-prices_df.columns = ['cdate_s','openp','highp','lowp','closep','volume','adjp']
 
-# I should setup prices3:
-pred1_sr   = prices_df.cdate_s >= '2016-08-01'
+prices_df         = pd.read_csv('http://ml4.us/csv/ibm.csv')
+prices_df.columns = ['cdate_s', 'openp', 'highp', 'lowp', 'closep', 'adjp', 'volume']
+
+# I should setup prices3_df:
+pred1a_sr  = prices_df.cdate_s > '2016-08-08'
+pred1b_sr  = prices_df.cdate_s < '2016-09-01'
+pred1_sr   = (pred1a_sr & pred1b_sr)
 prices3_df = prices_df.copy()[['cdate_s','openp']][pred1_sr]
 
 # I should report:
