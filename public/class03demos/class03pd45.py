8c96830d9799221a18a47037b18946e585af18bc
Dan Bikle <bikle@bikle.com>
2017-06-24 17:09:35 -0700

    more

--- public/class03demos/class03pd45.py
+++ public/class03demos/class03pd45.py
@@ -1,20 +1,22 @@
-# class03pd45.py
+"""
+class03pd45.py
 
-# This script should slice and dice
+This script should slice and dice
+"""
 
 import pandas as pd
 import numpy  as np
 
-prices_df = pd.read_csv('http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC')
-prices_df.columns = ['cdate_s','openp','highp','lowp','closep','volume','adjp']
+prices_df = pd.read_csv('http://ml4.us/csv/GSPC.csv')
+prices_df.columns = ['cdate_s','openp','highp','lowp','closep','adjp','volume']
 
 # I should get 2 columns and rows 5 through 11:
-s1_df = prices_df[['cdate_s','closep']][5:11]
+s1_df = prices_df[['cdate_s','closep']].iloc[5:11]
 print(s1_df)
 
 # I do same as above but in different order.
 # I should get rows 5 through 11 and 2 columns:
-s2_df = prices_df[5:11][['cdate_s','closep']]
+s2_df = prices_df.iloc[5:11][['cdate_s','closep']]
 print(s2_df)
 # s1_df and s2_df should have same data.
 
