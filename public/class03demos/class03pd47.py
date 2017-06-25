380efefcd4eeb3744d4be7439403ebd6a147a845
Dan Bikle <bikle@bikle.com>
2017-06-24 18:36:06 -0700

    more

--- public/class03demos/class03pd47.py
+++ public/class03demos/class03pd47.py
@@ -1,12 +1,14 @@
-# class03np15.py
+"""
+class03pd47.py
 
-# This script should slice and dice
+This script should slice and dice
+"""
 
 import pandas as pd
 import numpy  as np
 
-prices_df = pd.read_csv('http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC')
-prices_df.columns = ['cdate_s','openp','highp','lowp','closep','volume','adjp']
+prices_df = pd.read_csv('http://ml4.us/csv/GSPC.csv')
+prices_df.columns = ['cdate_s','openp','highp','lowp','closep','adjp','volume']
 
 # I should get 2016 July and two columns:
 pred_sr = (prices_df.cdate_s > '2016-07') & (prices_df.cdate_s < '2016-08')
