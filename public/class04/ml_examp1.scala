f8fc070ffc72d517799a868a51c650bc466b9038
Dan Bikle <danbikle@outlook.com>
2018-01-11 15:59:47 -0800

    more

--- public/class04/ml_examp1.scala
+++ public/class04/ml_examp1.scala
@@ -1,20 +1,31 @@
+/*
+I should enclose all syntax within curly-braces if I want to help
+spark-shell "see" expressions which span multiple lines:
+*/
+{ // Initial curly-brace for spark-shell.
 
-// ml_examp1.scala
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.linalg.{Vector, Vectors}
 import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.sql.Row
 
 // Prepare training data from a list of (label, features) tuples.
-val training = spark.createDataFrame(Seq((1.0, Vectors.dense(0.0, 1.1, 0.1)),  (0.0, Vectors.dense(2.0, 1.0, -1.0)),  (0.0, Vectors.dense(2.0, 1.3, 1.0)),  (1.0, Vectors.dense(0.0, 1.2, -0.5)))).toDF("label", "features")
+val training = spark.createDataFrame(Seq(
+  (1.0, Vectors.dense(0.0, 1.1, 0.1)),
+  (0.0, Vectors.dense(2.0, 1.0, -1.0)),
+  (0.0, Vectors.dense(2.0, 1.3, 1.0)),
+  (1.0, Vectors.dense(0.0, 1.2, -0.5))
+)).toDF("label", "features")
 
 // Create a LogisticRegression instance. This instance is an Estimator.
 val lr = new LogisticRegression()
 // Print out the parameters, documentation, and any default values.
 println("LogisticRegression parameters:\n" + lr.explainParams() + "\n")
 
+
 // We may set parameters using setter methods.
-lr.setMaxIter(10).setRegParam(0.01)
+lr.setMaxIter(10)
+  .setRegParam(0.01)
 
 // Learn a LogisticRegression model. This uses the parameters stored in lr.
 val model1 = lr.fit(training)
@@ -26,7 +37,9 @@ println("Model 1 was fit using parameters: " + model1.parent.extractParamMap)
 
 // We may alternatively specify parameters using a ParamMap,
 // which supports several methods for specifying parameters.
-val paramMap = ParamMap(lr.maxIter -> 20).put(lr.maxIter, 30).put(lr.regParam -> 0.1, lr.threshold -> 0.55)  // Specify multiple Params.
+val paramMap = ParamMap(lr.maxIter -> 20)
+  .put(lr.maxIter, 30)  // Specify 1 Param. This overwrites the original maxIter.
+  .put(lr.regParam -> 0.1, lr.threshold -> 0.55)  // Specify multiple Params.
 
 // One can also combine ParamMaps.
 val paramMap2 = ParamMap(lr.probabilityCol -> "myProbability")  // Change output column name.
@@ -38,12 +51,24 @@ val model2 = lr.fit(training, paramMapCombined)
 println("Model 2 was fit using parameters: " + model2.parent.extractParamMap)
 
 // Prepare test data.
-val test = spark.createDataFrame(Seq((1.0, Vectors.dense(-1.0, 1.5, 1.3)),  (0.0, Vectors.dense(3.0, 2.0, -0.1)),  (1.0, Vectors.dense(0.0, 2.2, -1.5)))).toDF("label", "features")
+val test = spark.createDataFrame(Seq(
+  (1.0, Vectors.dense(-1.0, 1.5, 1.3)),
+  (0.0, Vectors.dense(3.0, 2.0, -0.1)),
+  (1.0, Vectors.dense(0.0, 2.2, -1.5))
+)).toDF("label", "features")
 
 // Make predictions on test data using the Transformer.transform() method.
 // LogisticRegression.transform will only use the 'features' column.
 // Note that model2.transform() outputs a 'myProbability' column instead of the usual
 // 'probability' column since we renamed the lr.probabilityCol parameter previously.
-model2.transform(test).select("features", "label", "myProbability", "prediction").collect().foreach { case Row(features: Vector, label: Double, prob: Vector, prediction: Double) => println(s"($features, $label) -> prob=$prob, prediction=$prediction")  }
-
-//bye
+model2.transform(test)
+  .select("features", "label", "myProbability", "prediction")
+  .collect()
+  .foreach { case Row(features: Vector, label: Double, prob: Vector, prediction: Double) =>
+    println(s"($features, $label) -> prob=$prob, prediction=$prediction")
+  }
+} // Final curly-brace for spark-shell.
+/*
+I should enclose all syntax within curly-braces if I want to help
+spark-shell "see" expressions which span multiple lines.
+*/
