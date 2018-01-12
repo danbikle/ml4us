6a8b9fe53ab821d4eebde0947527cb8d77631f50
Dan Bikle <danbikle@outlook.com>
2018-01-11 22:36:50 -0800

    more

--- public/class04/linr10/linr11a.scala
+++ public/class04/linr10/linr11a.scala
@@ -15,6 +15,11 @@ import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.sql.Row
 import sys.process._
 
+// I should declare training_period, test_period
+
+val training_period = " WHERE Date BETWEEN'1986-01-01'AND'2016-01-01' "
+val test_period     = " WHERE Date BETWEEN'2016-01-01'AND'2017-01-01' "
+
 // I should get prices:
 "/usr/bin/curl -L ml4.us/csv/GSPC.csv -o /tmp/gspc.csv"!
 
@@ -31,7 +36,8 @@ dp10df.createOrReplaceTempView("tab")
 
 spark.sql("SELECT COUNT(Date),MIN(Date),MAX(Date),MIN(Close),MAX(Close)FROM tab").show
 
-// I should compute a label I can use to classify observations.
+// I should declare a dependent variable: "pctlead".
+// If it truely is dependent that makes it predictable.
 
 var sqls="SELECT Date,Close,LEAD(Close,1)OVER(ORDER BY Date) leadp FROM tab ORDER BY Date"
 
@@ -41,5 +47,34 @@ sqls="SELECT Date,Close,100*(leadp-Close)/Close pctlead FROM tab ORDER BY Date"
 
 val dp12df=spark.sql(sqls);dp12df.createOrReplaceTempView("tab")
 
+// I should now collect features AKA independent variables.
+
+sqls = "SELECT Date, Close, pctlead"
+sqls=sqls++",AVG(Close)OVER(ORDER BY Date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS mavg2"
+sqls=sqls++",AVG(Close)OVER(ORDER BY Date ROWS BETWEEN 3 PRECEDING AND CURRENT ROW) AS mavg3"
+sqls=sqls++",AVG(Close)OVER(ORDER BY Date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) AS mavg4"
+sqls=sqls++",AVG(Close)OVER(ORDER BY Date ROWS BETWEEN 5 PRECEDING AND CURRENT ROW) AS mavg5"
+sqls=sqls++",AVG(Close)OVER(ORDER BY Date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS mavg6"
+sqls=sqls++",AVG(Close)OVER(ORDER BY Date ROWS BETWEEN 7 PRECEDING AND CURRENT ROW) AS mavg7"
+sqls=sqls++",AVG(Close)OVER(ORDER BY Date ROWS BETWEEN 8 PRECEDING AND CURRENT ROW) AS mavg8"
+sqls=sqls++",AVG(Close)OVER(ORDER BY Date ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS mavg9"
+sqls=sqls++" FROM tab ORDER BY Date"
+
+val dp13df=spark.sql(sqls);dp13df.createOrReplaceTempView("tab")
+
+sqls = "SELECT Date, Close, pctlead"
+sqls=sqls++",(mavg2-LAG(mavg2,1)OVER(ORDER BY Date))/mavg2 AS slp2 "
+sqls=sqls++",(mavg3-LAG(mavg3,1)OVER(ORDER BY Date))/mavg3 AS slp3 "
+sqls=sqls++",(mavg3-LAG(mavg4,1)OVER(ORDER BY Date))/mavg3 AS slp4 "
+sqls=sqls++",(mavg3-LAG(mavg5,1)OVER(ORDER BY Date))/mavg3 AS slp5 "
+sqls=sqls++",(mavg3-LAG(mavg6,1)OVER(ORDER BY Date))/mavg3 AS slp6 "
+sqls=sqls++",(mavg3-LAG(mavg7,1)OVER(ORDER BY Date))/mavg3 AS slp7 "
+sqls=sqls++",(mavg3-LAG(mavg8,1)OVER(ORDER BY Date))/mavg3 AS slp8 "
+sqls=sqls++",(mavg3-LAG(mavg9,1)OVER(ORDER BY Date))/mavg3 AS slp9 "
+sqls=sqls++" FROM tab ORDER BY Date"
+
+val dp14df=spark.sql(sqls);dp14df.createOrReplaceTempView("tab")
+
+// For Class Boundry, I should get avg of pctlead over training period.
 
 }
