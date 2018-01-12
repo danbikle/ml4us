961b7a65d614c43b072c0a2252f259595d5a9374
Dan Bikle <danbikle@outlook.com>
2018-01-12 02:26:26 -0800

    more

--- public/class04/linr10/linr11a.py
+++ public/class04/linr10/linr11a.py
@@ -15,3 +15,14 @@ This script should implement the same ML idea as linr11a.scala:
 Demo:
 python linr11a.py
 """
+
+import pandas as pd
+import numpy  as np
+import pdb
+
+# I should Read ml4.us/csv/GSPC.csv into DataFrame
+gspc0_df = pd.read_csv('https://ml4.herokuapp.com/csv/GSPC.csv')
+
+# I should Generate dependent variable: "pctlead"
+leadp_sr   = gspc0_df.Close.shift(-1)
+pctlead_sr = (100.0*(leadp_sr - gspc0_df.Close)/gspc0_df.Close).fillna(0)
