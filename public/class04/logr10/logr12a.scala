b3268796bb4a901a2d22dafae5745caa18aa8f28
Dan Bikle <danbikle@outlook.com>
2018-01-11 19:11:58 -0800

    more

---
+++ public/class04/logr10/logr12a.scala
@@ -0,0 +1,13 @@
+{
+/* ~/sparkapps/logr10/logr12a.scala
+This script should download prices and predict daily direction of GSPC.
+It should generate a label which I assume to be dependent on price calculations.
+A label should classify an observation as down or up. Down is 0.0, up is 1.0.
+It should generate independent features from slopes of moving averages of prices.
+It should create a Logistic Regression model from many years of features.
+Demo:
+spark-shell -i logr12a.scala
+*/
+
+// UNDER CONSTRUCTION
+}
