daafb739117b6136e9cc752587ec3212b9011490
Dan Bikle <danbikle@outlook.com>
2018-01-12 12:31:17 -0800

    more

--- public/class04/logr10/logr12o.py
+++ public/class04/logr10/logr12o.py
@@ -6,8 +6,8 @@ This script should implement the same ML idea as logr12o.py.scala:
 - Read ml4.us/csv/GSPC.csv into DataFrame
 - Generate dependent variable: "pctlead"
 - Collect independent variables: slp2, ... slp9
-- Create train_df from a filter: 1986-01-01 until 2016-01-01
-- Create test_df  from a filter: 2016-01-01 until 2017-01-01
+- Create train_df        from a filter: 1986-01-01 until 2016-01-01
+- Create test_df,test_a  from a filter: 2016-01-01 until 2017-01-01
 - Calculate value of class_boundry_f from train_df.pctlead.mean()
 - Transform class_boundry_f into labels
 - Create train_a from train_df
@@ -39,17 +39,17 @@ for slope_i in [2,3,4,5,6,7,8,9]:
   slope_sr       = 100.0 * (rollx.mean().Close - rollx.mean().Close.shift(1))/rollx.mean().Close
   gspc_df[col_s] = slope_sr
 
-# I should Create train_df, train_a from a filter: 1986-01-01 until 2016-01-01
+# I should Create train_df from a filter: 1986-01-01 until 2016-01-01
 filter_train_sr = (gspc_df.Date > '1986') & (gspc_df.Date < '2016')
 columns_iwant_l = ['Date','pctlead','slp2','slp3','slp4','slp5','slp6','slp7','slp8','slp9']
 train_df        = gspc_df[columns_iwant_l].loc[filter_train_sr]
 
-# I should Create test_df,  test_a  from a filter: 2016-01-01 until 2017-01-01
+# I should Create test_df, test_a  from a filter: 2016-01-01 until 2017-01-01
 filter_test_sr = (gspc_df.Date > '2016') & (gspc_df.Date < '2017')
 test_df        = gspc_df[columns_iwant_l].loc[filter_test_sr]
+test_a         = np.array(test_df)
 
 # I should Calculate value of class_boundry_f from train_df.pctlead.mean()
-pdb.set_trace()
 class_boundry_f = train_df.pctlead.mean()
 # I should Transform class_boundry_f into labels
 labels_sr = train_df.pctlead > class_boundry_f
@@ -65,10 +65,13 @@ linr_model.fit(x_a, y_a)
 
 # I should Predict observations in test_a
 x_test_a      = test_a[:,2:]
-predictions_a = linr_model.predict(x_test_a)
+pdb.set_trace()
+bool_a        = linr_model.predict(x_test_a)
+predictions_a = linr_model.predict_proba(x_test_a)
 
 # I should Report accuracy and effectiveness of predictions
 rpt_df               = test_df[['Date','pctlead']].copy()
-rpt_df['prediction'] = predictions_a[:,0].tolist()
+rpt_df['prediction'] = predictions_a[:,1].tolist()
+
 
 'bye'
