0192b416701692b5cd0c9b18c13076aec30f829f
Dan Bikle <danbikle@outlook.com>
2018-01-11 19:13:32 -0800

    more

---
+++ public/class04/logr10/logr12e.scala
@@ -0,0 +1,32 @@
+{
+/* ~/sparkapps/logr10/logr12d.scala
+This script should download prices and predict daily direction of GSPC.
+It should generate a label which I assume to be dependent on price calculations.
+A label should classify an observation as down or up. Down is 0.0, up is 1.0.
+It should generate independent features from slopes of moving averages of prices.
+It should create a Logistic Regression model from many years of features.
+Demo:
+spark-shell -i logr12d.scala
+*/
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.ml.param.ParamMap
+import org.apache.spark.sql.Row
+import sys.process._
+
+// I should get prices:
+"/usr/bin/curl -L ml4.us/csv/GSPC.csv -o /tmp/gspc.csv"!
+
+val sqlContext = new SQLContext(sc)
+  
+val dp10df = sqlContext
+  .read
+  .format("com.databricks.spark.csv")
+  .option("header","true")
+  .option("inferSchema","true")
+  .load("/tmp/gspc.csv")
+
+// UNDER CONSTRUCTION
+}
