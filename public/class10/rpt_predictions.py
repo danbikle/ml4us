62f36c8daec8abbc89feaf38142cfbe982cc0398
Dan Bikle <danbikle@outlook.com>
2018-02-05 23:05:34 -0800

    more

--- public/class10/rpt_predictions.py
+++ public/class10/rpt_predictions.py
@@ -31,8 +31,40 @@ print(longonly_eff)
 print('model_eff/longonly_eff:')
 print(model_eff/longonly_eff)
 
+# I should create a plot:
+plot0_df          = predictions_df.iloc[:-1][['cdate','closep']]
+plot0_df['cdate'] = pd.to_datetime(plot0_df['cdate'], format='%Y-%m-%d')
+plot0_df.head()
+plot0_df.columns = ['cdate','Long_Only']
 
-plot0_df = predictions_df[:-1][['cdate','closep']]
+# I should create effectiveness-line for predictions.
+# I have two simple rules:
+# 1. If blue line moves $x, then model-line moves $x.
+# 2. If model is True, model-line goes up.
+
+len_i   = len(plot0_df)
+cp_sr   = plot0_df.Long_Only
+model_l = [cp_sr.iloc[0]]
+
+for row_i in range(len_i-1):
+    cp     = cp_sr.iloc[row_i]
+    cpnext = cp_sr.iloc[row_i+1]
+    cpdelt = cpnext - cp
+    modeldelt = np.sign(predictions_df.prediction.iloc[row_i])*cpdelt
+    model_l.append(model_l[row_i]+modeldelt)
+
+plot0_df['Model'] = model_l
+
+import matplotlib
+matplotlib.use('Agg')
+# Order is important here.
+# Do not move the next import:
+import matplotlib.pyplot as plt
+
+plot1_df = plot0_df.set_index(['cdate'])
+plot1_df.plot.line(title="Model Effectiveness Visualization", figsize=(11,7))
+plt.savefig(fn_s+'.png')
+plt.close()
 
 'bye'
 
