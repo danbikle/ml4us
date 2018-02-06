1228ea6cd99914b4e1374021ff7a1320f15c17f1
Dan Bikle <danbikle@outlook.com>
2018-02-05 19:17:26 -0800

    more

--- public/class10/sk_linr_learn_predict.py
+++ public/class10/sk_linr_learn_predict.py
@@ -16,11 +16,13 @@ train_i = 20 # amount of training years
 # I should loop through some years I want to predict:
 lastyr2predict_i = 2017
 for yr_i in range(2016,lastyr2predict_i+1):
-    yr_begin_i   = yr_i - train_i
-    predstart_sr = feat_df.cdate > str(yr_begin_i)
-    predend_sr   = feat_df.cdate < str(yr_i)
-    pred_sr      = predstart_sr & predend_sr
-    train_df     = feat_df[pred_sr]
-    print(train_df.tail(2))
+    yr_begin_i    = yr_i - train_i
+    trainstart_sr = feat_df.cdate > str(yr_begin_i)
+    trainend_sr   = feat_df.cdate < str(yr_i)
+    train_sr      = trainstart_sr & trainend_sr
+    train_df      = feat_df[train_sr]
+    test_sr       = feat_df.cdate.str.match(str(yr_i))
+    test_df       = feat_df[test_sr]
+    print(test_df.head(2))
 
 'bye'
