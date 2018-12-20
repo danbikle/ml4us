5baec15a0109b0aa0e43d341e977d1447a98b2bc
Dan Bikle <danbikle@outlook.com>
2018-12-19 19:16:07 -0800

    more

--- public/class03demos/class03np11.py
+++ public/class03demos/class03np11.py
@@ -1,11 +1,16 @@
-# class03np11.py
+"""
+class03np11.py
 
-# Create a NumPy Array From Pandas DataFrame of GSPC prices.
+This script should create a NumPy Array From Pandas DataFrame of GSPC prices.
+
+Demo:
+python class03np11.py
+"""
 
 import pandas as pd
 import numpy  as np
 
-prices_df = pd.read_csv('http://tkrprice.herokuapp.com/static/gspc.csv')
+prices_df = pd.read_csv('https://tkrprice.herokuapp.com/static/gspc.csv')
 prices_a  = np.array(prices_df)
 
 # I should print the first 9 rows to help me see the NumPy Array:
